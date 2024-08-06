d4747e6ef9bf55b742df6732267cd9aa5020dd32
ItzRazvan <cisteianrazvan@gmail.com>
2024-08-06 11:22:42 +0000

    game loop + print_at()

--- snake.c
+++ snake.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
 #include <sys/ioctl.h>
 
 #define UP 1
@@ -63,15 +64,67 @@ void obs_init(){
         generate_obs_coords(i);
 }
 
+typedef struct{
+    int score;
+    int max_score;
+    bool is_running;
+} Game;
+
+Game game;
 void game_init(){
+    game.score = 0;
+    game.max_score = 0;
+    game.is_running = 1;
+}
+
+
+char cursor_position[10];
+void cursor_position_init(){
+    cursor_position[0] = '\0';
+    cursor_position[1] = '3';
+    cursor_position[2] = '3';
+    cursor_position[3] = '[';
+    cursor_position[4] = (char)((int)'1' - (int)'0');
+    cursor_position[5] = ';';
+    cursor_position[7] = (char)((int)'1' - (int)'0');
+    cursor_position[8] = 'H';
+    cursor_position[9] = '\0';
+}
+
+void game_elements_init(){
     get_screen_measurements();
 
+    game_init();
     snake_init();
     obs_init();
+    cursor_position_init();
+}
+
+
+void clear_screen(){
+    printf("\033[H\033[J");
+}
+
+void print_at(int x, int y, char c){
+    printf("\033[%d;%dH%c", x, y, c);
+}
+
+void game_loop(){
+    while(game.is_running){
+        clear_screen();
+
+        print_at(4, 4, 'a');
+
+        if(!snake.alive)
+            game.is_running = 0;
+
+        usleep(80000);
+    }
 }
 
 int main(){
-    game_init();
+    game_elements_init();
+    game_loop();
 
     return 0;
 }
